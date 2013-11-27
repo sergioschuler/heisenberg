c63015d100e74eb8abc788b15f10453689607227
Sergio Schuler <sergio.schuler@gmail.com>
2013-11-27 20:14:35 -0200

    Remove wrong files created by mistake. Beutify app. PagSeguro working. All still a hackjob, just to ship it and validate the idea. Don't judge me by this code :)

--- app/controllers/tshirt_controller.rb
+++ app/controllers/tshirt_controller.rb
@@ -4,5 +4,8 @@ class TshirtController < ApplicationController
   end
 
   def buy
+  	if params[:gender] == nil || params[:size] == nil
+  		redirect_to root_url, notice: "Escolha um modelo e tamanho e clique em comprar."
+  	end
   end
 end
